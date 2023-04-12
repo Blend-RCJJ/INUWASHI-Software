3c61e3f4149186fa1e280475b6b54103cf4fa1f8
shirokuma89dev <code@shirokuma89.dev>
2023-04-12 16:23:59 +0900

    ジャイロ完了

--- F446-main/src/input/gyro.cpp
+++ F446-main/src/input/gyro.cpp
@@ -1,1 +1,73 @@
 #include "gyro.h"
+
+GYRO::GYRO(Adafruit_BNO055 *p) {
+    sensorPtr = p;
+}
+
+void GYRO::init(void) {
+    sensorPtr->begin(OPERATION_MODE_IMUPLUS);
+
+    int eeAddress = 0;
+    long bnoID;
+    bool foundCalib = false;
+
+    EEPROM.get(eeAddress, bnoID);
+
+    adafruit_bno055_offsets_t calibrationData;
+    sensor_t sensor;
+
+    sensorPtr->getSensor(&sensor);
+
+    if (bnoID == sensor.sensor_id) {
+        eeAddress += sizeof(long);
+        EEPROM.get(eeAddress, calibrationData);
+        sensorPtr->setSensorOffsets(calibrationData);
+        foundCalib = true;
+    }
+
+    uint8_t system_status, self_test_results, system_error;
+    system_status = self_test_results = system_error = 0;
+    sensorPtr->getSystemStatus(&system_status, &self_test_results,
+                               &system_error);
+
+    sensorPtr->setExtCrystalUse(true);
+}
+
+int GYRO::read(void) {
+    sensors_event_t event;
+    sensorPtr->getEvent(&event);
+
+    if (isGyroDisabled) {  // 地磁気
+        magnetic = event.magnetic.x;
+        deg = (int)(magnetic - offset + 360) % 360;
+    } else {
+        deg = (int)(event.orientation.x - offset + 360) % 360;
+    }
+
+    slope = (int)(event.orientation.z - slopeOffset + 360) % 360;
+
+    if (slope >= 180) {
+        slope -= 360;
+    }
+    slope *= -1;
+
+    if (abs(slope) <= 8) {
+        slope = 0;
+    }
+
+    return deg;
+}
+
+void GYRO::setOffset(void) {
+    sensors_event_t event;
+    sensorPtr->getEvent(&event);
+    offset = event.magnetic.x;
+
+    if (isGyroDisabled) {  // 地磁気
+        offset = event.magnetic.x;
+    } else {
+        offset = event.orientation.x;
+    }
+
+    slopeOffset = event.orientation.z;
+}
--- F446-main/src/input/gyro.h
+++ F446-main/src/input/gyro.h
@@ -1,8 +1,34 @@
 #ifndef _GYRO_H
 #define _GYRO_H
 #include "Arduino.h"
+
+#include <SPI.h>
+#include <Wire.h>
+#include <EEPROM.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
 class GYRO {
    public:
+    GYRO(Adafruit_BNO055 *p);
+    Adafruit_BNO055 *sensorPtr;
+    
+    int deg = 0;
+    int magnetic = 0;
+    int offset = 0;
+
+    int error = 0;
+    int oldDeg = 0;
+
+    int slope = 0;
+    int slopeOffset = 0;
+
+    bool isGyroDisabled = true;
+
+    void init(void);
+    void setOffset(void);
+    int read(void);
+
    private:
 };
 
