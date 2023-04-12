469718463af902d8057403fc24d1e1f9cff162fe
onagi37s <suminagiotibi@icloud.com>
2023-04-12 16:27:50 +0900

    床面センサできた

--- F446-main/src/input/floorSensor.h
+++ F446-main/src/input/floorSensor.h
@@ -1,8 +1,19 @@
 #ifndef _FLOOR_SENSOR_H
 #define _FLOOR_SENSOR_H
+
+#if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
 class FLOOR_SENSOR {
    public:
+    const int brightness = 200;
+    int redVal;
+    int greenVal;
+    int blueVal;
+
    private:
 };
 
