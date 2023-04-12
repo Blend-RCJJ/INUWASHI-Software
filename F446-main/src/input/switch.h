bdf0d7ab27f33461cef44e0c053f791af05df09c
onagi37s <suminagiotibi@icloud.com>
2023-04-12 16:19:43 +0900

    ブザーできた(音階とbeat関数生きてる)

--- F446-main/src/input/switch.h
+++ F446-main/src/input/switch.h
@@ -12,10 +12,10 @@
 class SWITCH {
    public:
     Input buttonPin[2] = {PA4, PA5};
-    Input togglePin = PA6;
+    Input togglePin    = PA6;
 
     bool button[2] = {false, false};
-    bool toggle = false;
+    bool toggle    = false;
 
     void read(void) {
         for (int i = 0; i < 2; i++) {
--- F446-main/src/output/buzzer.cpp
+++ F446-main/src/output/buzzer.cpp
@@ -1,1 +1,13 @@
 #include "buzzer.h"
+
+void BUZZER::beat(int freq, double beat) {
+    int interval = 2;
+    if (freq == 0) {
+        mute();
+    } else {
+        setFrequency(freq);
+    }
+    delay((60000.0 / bpm) * beat - interval);
+    mute();
+    delay(interval);
+}
