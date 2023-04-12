8f6ef3b565d5b32063ac8ced3ebe4c774c56e1bf
shirokuma89dev <code@shirokuma89.dev>
2023-04-12 16:11:16 +0900

    距離せんさできたお！

--- F446-main/src/input/distanceSensor.h
+++ F446-main/src/input/distanceSensor.h
@@ -1,9 +1,24 @@
 #ifndef _DISTANCE_SENSOR_H
 #define _DISTANCE_SENSOR_H
-#include "Arduino.h"
+
+#include <Arduino.h>
+
 class DISTANCE_SENSOR {
    public:
+    DISTANCE_SENSOR(HardwareSerial *p);
+
+    HardwareSerial *serialPtr;
+
+    int val[12] = {0};
+
+    int vecX[12] = {0};
+    int vecY[12] = {0};
+
+    int read(void);
+    void calcVector(int angle);  // 定義しときました〜
+
    private:
+    int _valTemp[12] = {0};
 };
 
 #endif
