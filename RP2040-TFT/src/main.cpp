c24da337f2aecfdb4be1945c2dde8342d10e0204
shirokuma89dev <code@shirokuma89.dev>
2023-05-23 23:48:49 +0900

    タッチ実装

--- RP2040-TFT/src/main.cpp
+++ RP2040-TFT/src/main.cpp
@@ -8,6 +8,10 @@ XPT2046_Touchscreen ts(20);
 #include "img/blend.h"
 #include "img/shirosuke.h"
 #include "img/shirokuma.h"
+#include "img/samba.h"
+#include "img/top2.h"
+#include "img/led.h"
+#include "img/matsuken.h"
 
 #define ARR_SIZE 76800
 
@@ -43,40 +47,88 @@ void setup() {
 
     ts.begin();
     ts.setRotation(3);
-
-    showBootImage();
 }
+
+int mode = 0;
+int oldMode = 1;
 
 void loop() {
     bool bTouch = ts.touched();
+    static bool oldTouch = false;
     TS_Point p = ts.getPoint();
 
+    static int oldY = 0;
+    static int homeCount = 0;
+    static int homeTime = 0;
     delay(10);
 
     if (bTouch) {
-        Serial.print("x:");
+        if (!oldTouch && mode == 0) {
+            if (abs(3000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+                mode = 1;
+            } else if (abs(1000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+                mode = 2;
+            } else if (abs(2000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+                mode = 3;
+            } else if (abs(1000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+                mode = 4;
+            } else if (abs(2000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+                mode = 5;
+            } else if (abs(3000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+                mode = 6;
+            }
+        }
+        
+        if (mode != 0) {
+            if (abs(2000 - p.x) < 800 && p.y > 3000 && !oldTouch) {
+                homeCount = 1;
+                oldY = p.y;
+                homeTime = millis();
+            }
+
+            if (homeCount == 1 && millis() - homeTime < 100) {
+                if (p.y < oldY - 300) {
+                    homeCount = 0;
+                    mode = 0;
+                }
+            }
+        }
+
         Serial.print(p.x);
-        Serial.print("\ty:");
+        Serial.print("\t");
         Serial.println(p.y);
+    }
+    oldTouch = bTouch;
 
+    if (mode != oldMode) {
         SPI.beginTransaction(
             SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
         SPI.endTransaction();
 
-        showImage(image_data_shirokuma);
+        switch (mode) {
+            case 0:
+                showImage(image_data_top);
+                break;
+            case 1:
+                showImage(image_data_led);
+                break;
+            case 2:
+                showImage(image_data_samba);
+                break;
+            case 3:
+                showImage(image_data_shirokuma);
+                break;
+            case 4:
+                showImage(image_data_blend);
+                break;
+            case 5:
+                showImage(image_data_shirosuke);
+                break;
+            case 6:
+                showImage(image_data_matsuken);
+                break;
+        }
 
-        tft.setCursor(0, 0);
-        tft.setTextColor(TFT_RED);
-        tft.setTextSize(8);
-        tft.print(p.x);
-        tft.setCursor(0, 15 * 4);
-        tft.print(p.y);
-    } else {
-        Serial.println("No YUBI!!!!!");
-        SPI.beginTransaction(
-            SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
-        SPI.endTransaction();
-
-        showImage(image_data_blend);
+        oldMode = mode;
     }
 }
