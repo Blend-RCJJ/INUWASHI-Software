f4b7f67bfa0f8a8b1079968e17b68e7db9e41e5c
shirokuma89dev <code@shirokuma89.dev>
2023-06-02 16:36:39 +0900

    ディスプレイかっこいいね！

--- RP2040-TFT/src/main.cpp
+++ RP2040-TFT/src/main.cpp
@@ -6,19 +6,25 @@ TFT_eSPI tft = TFT_eSPI();
 XPT2046_Touchscreen ts(20);
 
 #include "img/blend.h"
-#include "ui-kit/00-top.h"
-#include "ui-kit/01-camera.h"
-#include "ui-kit/02-led.h"
-#include "ui-kit/03-motor.h"
-#include "ui-kit/04-load.h"
-#include "ui-kit/05-tof.h"
-#include "ui-kit/06-floor.h"
+// #include "ui-kit/00-top.h"
+// #include "ui-kit/01-camera.h"
+// #include "ui-kit/02-led.h"
+// #include "ui-kit/03-motor.h"
+// #include "ui-kit/04-load.h"
+// #include "ui-kit/05-tof.h"
+// #include "ui-kit/06-floor.h"
 // #include "img/shirosuke.h"
 // #include "img/shirokuma.h"
 // #include "img/samba.h"
 // #include "img/top3.h"
 // #include "img/led.h"
 // #include "img/matsuken.h"
+#include "victim/victim-u.h"
+#include "victim/victim-u-kit.h"
+#include "victim/victim-red.h"
+#include "victim/victim-red-kit.h"
+#include "victim/victim-h.h"
+#include "victim/victim-h-kit.h"
 
 #define ARR_SIZE 76800
 
@@ -52,88 +58,125 @@ void setup() {
     ts.setRotation(3);
 
     showBootImage();
+
+    while (1) {
+        if (Serial.available()) {
+            char data = Serial.read();
+            if (data == 'u') {
+                while (Serial.read() != 'd') {
+                    // for (int i = 0; i < 2; i++) {
+                    showImage(victimu);
+                    delay(1000);
+                    showImage(victimukit);
+                    delay(1000);
+                    // }
+                }
+                showBootImage();
+            } else if (data == 'r') {
+                while (Serial.read() != 'd') {
+                    // for (int i = 0; i < 2; i++) {
+                    showImage(victimred);
+                    delay(1000);
+                    showImage(victimredkit);
+                    delay(1000);
+                    // }
+                }
+                showBootImage();
+            } else if (data == 'h') {
+                while (Serial.read() != 'd') {
+                    // for (int i = 0; i < 2; i++) {
+                    showImage(victimh);
+                    delay(1000);
+                    showImage(victimhkit);
+                    delay(1000);
+                    // }
+                }
+                showBootImage();
+            }
+        }
+    }
 }
 
 int mode = 0;
 int oldMode = 1;
 
 void loop() {
-    bool bTouch = ts.touched();
-    static bool oldTouch = false;
-    TS_Point p = ts.getPoint();
+    // bool bTouch = ts.touched();
+    // static bool oldTouch = false;
+    // TS_Point p = ts.getPoint();
 
-    static int oldY = 0;
-    static int homeCount = 0;
-    static int homeTime = 0;
-    delay(10);
+    // static int oldY = 0;
+    // static int homeCount = 0;
+    // static int homeTime = 0;
+    // delay(10);
 
-    if (bTouch) {
-        if (!oldTouch && mode == 0) {
-            if (abs(1000 - p.x) < 500 && abs(3000 - p.y) < 500) {
-                mode = 4;
-            } else if (abs(2000 - p.x) < 500 && abs(3000 - p.y) < 500) {
-                mode = 5;
-            } else if (abs(3000 - p.x) < 500 && abs(3000 - p.y) < 500) {
-                mode = 6;
-            } else if (abs(1000 - p.x) < 500 && abs(1600 - p.y) < 500) {
-                mode = 1;
-            } else if (abs(2000 - p.x) < 500 && abs(1600 - p.y) < 500) {
-                mode = 2;
-            } else if (abs(3000 - p.x) < 500 && abs(1600 - p.y) < 500) {
-                mode = 3;
-            }
-        }
-        
-        if (mode != 0) {
-            if (abs(2000 - p.x) < 800 && p.y > 3000 && !oldTouch) {
-                homeCount = 1;
-                oldY = p.y;
-                homeTime = millis();
-            }
+    // if (bTouch) {
+    //     if (!oldTouch && mode == 0) {
+    //         if (abs(1000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+    //             mode = 4;
+    //         } else if (abs(2000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+    //             mode = 5;
+    //         } else if (abs(3000 - p.x) < 500 && abs(3000 - p.y) < 500) {
+    //             mode = 6;
+    //         } else if (abs(1000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+    //             mode = 1;
+    //         } else if (abs(2000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+    //             mode = 2;
+    //         } else if (abs(3000 - p.x) < 500 && abs(1600 - p.y) < 500) {
+    //             mode = 3;
+    //         }
+    //     }
 
-            if (homeCount == 1 && millis() - homeTime < 100) {
-                if (p.y < oldY - 300) {
-                    homeCount = 0;
-                    mode = 0;
-                }
-            }
-        }
+    //     if (mode != 0) {
+    //         if (abs(2000 - p.x) < 800 && p.y > 3000 && !oldTouch) {
+    //             homeCount = 1;
+    //             oldY = p.y;
+    //             homeTime = millis();
+    //         }
 
-        Serial.print(p.x);
-        Serial.print("\t");
-        Serial.println(p.y);
-    }
-    oldTouch = bTouch;
+    //         if (homeCount == 1 && millis() - homeTime < 100) {
+    //             if (p.y < oldY - 300) {
+    //                 homeCount = 0;
+    //                 mode = 0;
+    //             }
+    //         }
+    //     }
 
-    if (mode != oldMode) {
-        SPI.beginTransaction(
-            SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
-        SPI.endTransaction();
+    //     Serial.print(p.x);
+    //     Serial.print("\t");
+    //     Serial.println(p.y);
+    // }
+    // oldTouch = bTouch;
 
-        switch (mode) {
-            case 0:
-                showImage(image_data_00top);
-                break;
-            case 1:
-                showImage(image_data_01camera);
-                break;
-            case 2:
-                showImage(image_data_02led);
-                break;
-            case 3:
-                showImage(image_data_03motor);
-                break;
-            case 4:
-                showImage(image_data_04load);
-                break;
-            case 5:
-                showImage(image_data_05tof);
-                break;
-            case 6:
-                showImage(image_data_06floor);
-                break;
-        }
+    // if (mode != oldMode) {
+    //     SPI.beginTransaction(
+    //         SPISettings(SPI_FREQUENCY, MSBFIRST, TFT_SPI_MODE));
+    //     SPI.endTransaction();
 
-        oldMode = mode;
-    }
+    //     switch (mode) {
+    //         case 0:
+    //             showImage(image_data_00top);
+    //             break;
+    //         case 1:
+    //             showImage(image_data_01camera);
+    //             break;
+    //         case 2:
+    //             showImage(image_data_02led);
+    //             break;
+    //         case 3:
+    //             showImage(image_data_03motor);
+    //             break;
+    //         case 4:
+    //             showImage(image_data_04load);
+    //             break;
+    //         case 5:
+    //             showImage(image_data_05tof);
+    //             break;
+    //         case 6:
+    //             showImage(image_data_06floor);
+    //             break;
+    //     }
+
+    //     oldMode = mode;
+    // }
 }
