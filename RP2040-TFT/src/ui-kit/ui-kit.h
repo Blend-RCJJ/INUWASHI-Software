03f768a204e71677f3b7e123b1e094b1429e33a5
shirokuma89dev <code@shirokuma89.dev>
2023-06-09 00:58:39 +0900

    fawe

---
+++ RP2040-TFT/src/ui-kit/ui-kit.h
@@ -0,0 +1,12 @@
+#ifndef _UI_KIT_H_
+#define _UI_KIT_H_
+
+#include "ui-kit/00-top.h"
+#include "ui-kit/01-camera.h"
+#include "ui-kit/02-led.h"
+#include "ui-kit/03-motor.h"
+#include "ui-kit/04-load.h"
+#include "ui-kit/05-tof.h"
+#include "ui-kit/06-floor.h"
+
+#endif
